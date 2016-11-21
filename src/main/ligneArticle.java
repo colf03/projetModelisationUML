63566d6f32772f5b319c2fbdef086cc3343b790a
pc-florian\Florian <collyflorian39@gmail.com>
2016-11-21 14:29:59 -0500

    ajout des premiers tables

--- src/main/ligneArticle.java
+++ src/main/ligneArticle.java
@@ -1,3 +1,5 @@
+package main;
+
 
 public class ligneArticle {
 	private int quantite ;
--- src/main/videothequeVendable.java
+++ src/main/videothequeVendable.java
@@ -1,3 +1,5 @@
+package main;
+
 public class videothequeVendable extends videotheque {
 	
 	private float prix ; 
