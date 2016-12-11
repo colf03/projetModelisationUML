6aa5f1045ab3e65fdd68a9fb0bc834f62dc3ea18
Shushan <noname320@hotmail.com>
2016-12-11 02:29:05 -0500

    Renamed some source files, since it was causing conflicts. (Fucking git working in case insensitive...)

---
+++ src/main/Magasin.java
@@ -0,0 +1,17 @@
+package main;
+
+
+public class Magasin {
+
+	private String nom ;
+	
+	private String adresse ;
+
+public Magasin ()  {
+	
+	this.nom=null ;
+	this.adresse=null ; 
+	
+}
+
+}
---
+++ src/main/VideothequeVendable.java
@@ -0,0 +1,14 @@
+package main;
+
+public class VideothequeVendable extends Videotheque {
+	
+	private float prix ; 
+	
+	public VideothequeVendable ()
+	{
+	
+	  super () ;
+	  
+	  this.prix= 0 ;
+	}
+}
